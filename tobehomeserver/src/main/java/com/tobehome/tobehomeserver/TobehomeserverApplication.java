c36b55846e35f61772d9841580994ceb12e4a22e
bykbyk0401 <bykbyk0401@gmail.com>
2024-02-02 21:10:26 +0900

    setting: 프로젝트 세팅

---
+++ tobehomeserver/src/main/java/com/tobehome/tobehomeserver/TobehomeserverApplication.java
@@ -0,0 +1,13 @@
+package com.tobehome.tobehomeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobehomeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobehomeserverApplication.class, args);
+	}
+
+}
---
+++ tobehomeserver/src/test/java/com/tobehome/tobehomeserver/TobehomeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobehome.tobehomeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobehomeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

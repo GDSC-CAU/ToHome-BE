1d8b4d9058b9efb7d9567d43407b3b5ad78f09dd
llswll023 <llswll023@gmail.com>
2024-02-07 21:08:49 +0900

    feat: post 초기, 업뎃/삭제 필요 커밋

---
+++ tobehomeserver/src/main/java/com/tobehome/tobehomeserver/controller/PostController.java
@@ -0,0 +1,13 @@
+package com.tobehome.tobehomeserver.controller;
+
+//import com.tobehome.tobehomeserver.dto.request.post;
+import com.tobehome.tobehomeserver.service.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.*;
+
+public class PostController {
+}
---
+++ tobehomeserver/src/main/java/com/tobehome/tobehomeserver/domain/entity/Post.java
@@ -0,0 +1,18 @@
+package com.tobehome.tobehomeserver.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+public class Post {
+
+}
